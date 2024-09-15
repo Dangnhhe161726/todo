178d2824fbe97a4167c88ffb330e136fd15f971f
Groot2308 <danghoan2382002@gmail.com>
2024-09-15 23:12:19 +0700

    UserController

---
+++ TodoAppDeploy/TodoAppDeploy/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TodoAppDeploy.Models;
+
+namespace TodoAppDeploy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly todoappdeployContext _context;
+        public UserController(todoappdeployContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetAllUser")]
+        public IActionResult Get()
+        {
+            var listUser = _context.Users.ToList();
+            return Ok(listUser);
+        }
+    }
+}
